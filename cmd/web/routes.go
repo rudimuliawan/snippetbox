d07786aa9bf88ee92b126138c7c4e49690eefaca
Rudi Muliawan <muliawan.rudi91@gmail.com>
2025-03-16 21:56:15 +0700

    add custom middlewars

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
@@ -12,5 +15,7 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("POST /snippet/create", app.snippetCreate)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreatePost)
 
-	return mux
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	return standard.Then(mux)
 }
